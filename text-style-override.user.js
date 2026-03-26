86ad3b95de2f1d93d288331e14ae6ac8c2e925cf
Lee Seung Chan <145134141+ppodori@users.noreply.github.com>
2026-03-26 16:49:46 +0900

    Add update and download URLs
    Added update and download URLs for the user script.

--- text-style-override.user.js
+++ text-style-override.user.js
@@ -5,6 +5,8 @@
 // @description  자간 조절 및 글씨 외곽선 적용 (성능 최적화 및 가독성 개선)
 // @match        *://*/*
 // @run-at       document-start
+// @updateURL    https://raw.githubusercontent.com/ppodori/my-userscripts/main/text-style-override.user.js
+// @downloadURL  https://raw.githubusercontent.com/ppodori/my-userscripts/main/text-style-override.user.js
 // ==/UserScript==
 
 (function () {
