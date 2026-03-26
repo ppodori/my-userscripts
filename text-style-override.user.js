1861d19788269bd04bcf61e0d0a6e87279a8a1ac
Lee Seung Chan <145134141+ppodori@users.noreply.github.com>
2026-03-26 19:52:12 +0900

    text-style-override.user.js 업데이트

--- text-style-override.user.js
+++ text-style-override.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Text Style Override (iOS Safari Optimized)
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @description  자간 조절 및 글씨 외곽선 적용 (성능 최적화 및 가독성 개선)
 // @match        *://*/*
 // @run-at       document-start
@@ -21,7 +21,7 @@
         select, 
         textarea { 
             letter-spacing: -0.5px !important; 
-            -webkit-text-stroke-width: 0.5px !important; 
+            -webkit-text-stroke-width: 0.4px !important; 
         }
     `;
     
