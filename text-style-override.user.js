caedc1aca1693bbb88f5866e275fad514c92c502
Lee Seung Chan <145134141+ppodori@users.noreply.github.com>
2026-04-01 18:14:27 +0900

    Update metadata for Text Style Override script

--- text-style-override.user.js
+++ text-style-override.user.js
@@ -1,13 +1,16 @@
 // ==UserScript==
 // @name         Text Style Override (iOS Safari Optimized)
-// @namespace    http://tampermonkey.net/
-// @version      1.5
+// @namespace    https://github.com/ppodori
+// @author       ppodori
+// @homepageURL  https://github.com/ppodori/my-userscripts
+// @version      1.6
 // @description  자간 조절 및 글씨 외곽선 적용 (성능 최적화 및 가독성 개선)
 // @match        *://*/*
 // @run-at       document-start
 // @updateURL    https://raw.githubusercontent.com/ppodori/my-userscripts/main/text-style-override.user.js
 // @downloadURL  https://raw.githubusercontent.com/ppodori/my-userscripts/main/text-style-override.user.js
 // ==/UserScript==
+
 
 (function () {
     'use strict';
